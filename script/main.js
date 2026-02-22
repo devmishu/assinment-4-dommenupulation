ae7b5e095b499434ba865bbbf78d7345561f34c0
Mishu <mishudebnath927@gmail.com>
2026-02-22 11:14:39 +0600

    delete element function emplement

--- script/main.js
+++ script/main.js
@@ -1,36 +1,39 @@
 
 // global variable
 
+// interview and reject
+const interviewList = [];
+const rejectList = [];
+
 // totaljob Element
 let jobListElement = document.getElementById('job-list');
 let totalJobElement = document.getElementById('total-job');
 let availableJobsElement = document.getElementById('available-jobs');
+let countTotalJob = 0;
 
 // toggle button Element
 let allJobElement = document.getElementById('all-job-btn');
 let interviewElement = document.getElementById('interview-btn');
 let rejectedElement = document.getElementById('rejected-btn');
 
-let nojobsElement = document.getElementById('nojobs');
+let noJobsElement = document.getElementById('nojobs');
+
+const mainContainer = document.querySelector('main');
+console.log(mainContainer);
 
 
 
 // total job count function
-function countTotalJob() {
-    let countTotalJob = 0;
-    for (const element of jobListElement.children) {
-        countTotalJob++;
-    }
+function countTotalJobFunc() {
+    countTotalJob = jobListElement.children.length;
     totalJobElement.innerText = countTotalJob;
     availableJobsElement.innerText = `${countTotalJob} Jobs`;
-
-
 }
-countTotalJob();
 
 
+// toggle navigat function
 function toggleStyle(id) {
-    allJobElement.classList.add('text-white', 'text-gray-500' );
+    allJobElement.classList.add('text-gray-500');
     allJobElement.classList.remove('bg-blue-600', 'text-white');
     interviewElement.classList.remove('bg-blue-600', 'text-white');
     rejectedElement.classList.remove('bg-blue-600', 'text-white');
@@ -41,14 +44,14 @@ function toggleStyle(id) {
 
     if (id === 'interview-btn') {
         jobListElement.classList.add('hidden');
-        nojobsElement.classList.remove('hidden');
-        
+        noJobsElement.classList.remove('hidden');
+
     }
 
     if (id === 'rejected-btn') {
         jobListElement.classList.add('hidden');
-        nojobsElement.classList.remove('hidden');
-        
+        noJobsElement.classList.remove('hidden');
+
     }
 
     if (id === 'all-job-btn') {
@@ -56,3 +59,17 @@ function toggleStyle(id) {
     }
 
 }
+
+// element Delete function
+function deleteJobElement() {
+    mainContainer.addEventListener('click', function (event) {
+        const jobElement = event.target.parentNode.parentNode;
+        if (event.target.classList.contains('delete-btn')) {
+            jobElement.remove();
+            countTotalJobFunc();
+        }
+    });
+}
+
+deleteJobElement();
+countTotalJobFunc();
